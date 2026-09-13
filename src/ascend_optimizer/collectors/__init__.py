bc01c7a495a3477349fdfca7ba79cdf1e3a7fec0
Ravinjeet Singh <75482385+Ravin1010@users.noreply.github.com>
2026-09-13 13:14:13 +0800

    Export Merkl incentive helper

--- src/ascend_optimizer/collectors/__init__.py
+++ src/ascend_optimizer/collectors/__init__.py
@@ -2,12 +2,14 @@
 
 from .gimo import collect_gimo_snapshot
 from .jaine import collect_jaine_snapshot
+from .merkl import fetch_merkl_pool_incentive
 from .native_staking import collect_native_staking_snapshot
 from .oku import collect_oku_snapshot
 
 __all__ = [
     "collect_gimo_snapshot",
     "collect_jaine_snapshot",
+    "fetch_merkl_pool_incentive",
     "collect_native_staking_snapshot",
     "collect_oku_snapshot",
 ]
