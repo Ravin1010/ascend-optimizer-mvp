49260bc0fe4d63edce23c2bbcc2a9d88d9a80469
Ravinjeet Singh <75482385+Ravin1010@users.noreply.github.com>
2026-09-14 11:28:25 +0800

    Configure Hardhat Solidity tests

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-ethers");
+
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test-contracts",
+    cache: "./cache-contracts",
+    artifacts: "./artifacts-contracts"
+  }
+};
